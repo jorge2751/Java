ea770533e0e7770759601656751eb199c267132a
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-04-16 23:16:01 -0700

    Update DaikichiController.java

--- DaikichiRoutes/src/main/java/com/jorge/daikichiroutes/controllers/DaikichiController.java
+++ DaikichiRoutes/src/main/java/com/jorge/daikichiroutes/controllers/DaikichiController.java
@@ -1,6 +1,7 @@
 package com.jorge.daikichiroutes.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,21 @@ public class DaikichiController {
 	@GetMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@GetMapping("/travel/{location}")
+	public String travelTo(@PathVariable("location") String location) {
+		return "Congratulations! Youy will soon travel to " + location + "!";
+	}
+	
+	@GetMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") int num) {
+		if (num % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+		}
+		
 	}
 
 }
